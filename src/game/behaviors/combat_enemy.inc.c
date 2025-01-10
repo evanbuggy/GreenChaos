232f74121009d19fff8bf6c1c3c4a2f075c1bf85
Buggy <evanbuggy15@gmail.com>
2025-01-10 17:01:12 +0000

    Variable Health

--- src/game/behaviors/combat_enemy.inc.c
+++ src/game/behaviors/combat_enemy.inc.c
@@ -2,7 +2,7 @@ static struct ObjectHitbox sCombatEnmemyHitbox = {
     /* interactType:      */ INTERACT_BOUNCE_TOP,
     /* downOffset:        */ 0,
     /* damageOrCoinValue: */ 1,
-    /* health:            */ 0,
+    /* health:            */ 5,
     /* numLootCoins:      */ 1,
     /* radius:            */ 72,
     /* height:            */ 50,
@@ -23,5 +23,6 @@ void bhv_combat_enemy_loop(void) {
     if (obj_handle_attacks(&sCombatEnmemyHitbox, o->oAction, sCombatEnemyAttackHandlers)) {
         play_sound(SOUND_GENERAL_COLLECT_1UP, gGlobalSoundSource);
         obj_die_if_health_non_positive();
+        o->oHealth--;
     }
 }
