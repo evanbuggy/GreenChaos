2bdfbbc112c554880e42d5111d13b9dad3dffdc0
frijoles.z64 <135540428+FrijolesYQueso@users.noreply.github.com>
2025-01-22 14:58:22 -0600

    Level Stuffs

--- levels/lll/area_1/macro.inc.c
+++ levels/lll/area_1/macro.inc.c
@@ -1,6 +1,9 @@
 const MacroObject lll_area_1_macro_objs[] = {
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -17767, 3173, -20740, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -17767, 3173, -20689, 0x00000000),
 	MACRO_OBJECT_WITH_BEH_PARAM(macro_red_coin, -90, -17943, 3716, -22098, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -13434, 4207, -20689, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -11725, 4533, -20689, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 180, -10283, 4533, -18730, 0x00000000),
 	MACRO_OBJECT_END(),
 };
 
