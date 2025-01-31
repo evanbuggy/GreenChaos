64cb9f7879f1994d8f2cb676b378c26c10d78130
Buggy <evanbuggy15@gmail.com>
2025-01-31 20:59:48 +0000

    i forgor to save interaction.c for coin combo timers

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -737,7 +737,7 @@ void reset_mario_pitch(struct MarioState *m) {
 u32 interact_coin(struct MarioState *m, UNUSED u32 interactType, struct Object *obj) {
     m->numCoins += obj->oDamageOrCoinValue;
     m->combo++;
-    m->comboTimer = 150;
+    m->comboTimer = 200;
     m->healCounter += 4 * obj->oDamageOrCoinValue;
 #ifdef BREATH_METER
     m->breathCounter += (4 * obj->oDamageOrCoinValue);
