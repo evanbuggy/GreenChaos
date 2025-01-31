81e5175703109c675528c94bc7b934f3e3c80468
Buggy <evanbuggy15@gmail.com>
2025-01-31 01:11:58 +0000

    More speed highway

--- levels/wf/texscroll.inc.h
+++ levels/wf/texscroll.inc.h
@@ -1,3 +1,4 @@
 extern void scroll_wf_dl_ArrowSIgn_mesh_layer_1_vtx_0();
 extern void scroll_wf_dl_ArrowSign2_mesh_layer_1_vtx_0();
+extern void scroll_wf_dl_ArrowSign2_001_mesh_layer_1_vtx_0();
 extern void scroll_wf();
