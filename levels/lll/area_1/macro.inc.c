0b9783888dfd86cb820152b9d6834ff58121f2b2
Frijoles Y Queso <135540428+FrijolesYQueso@users.noreply.github.com>
2025-02-02 21:40:20 -0600

    LLL work again

--- levels/lll/area_1/macro.inc.c
+++ levels/lll/area_1/macro.inc.c
@@ -1,9 +1,9 @@
 const MacroObject lll_area_1_macro_objs[] = {
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -17767, 3173, -20689, 0x00000000),
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_red_coin, -90, -17943, 3716, -22098, 0x00000000),
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -13434, 4207, -20689, 0x00000000),
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, -11725, 4533, -20689, 0x00000000),
-	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 180, -10283, 4533, -18730, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, 9230, 3173, -20689, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_red_coin, -90, 9054, 3716, -22098, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, 13563, 4207, -20689, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, -90, 15272, 4533, -20689, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 180, 16715, 4533, -18730, 0x00000000),
 	MACRO_OBJECT_END(),
 };
 
--- levels/lll/texscroll.inc.c
+++ levels/lll/texscroll.inc.c
@@ -1,3 +1,24 @@
+void scroll_lll_dl_Level_001_mesh_layer_1_vtx_8() {
+	int i = 0;
+	int count = 14;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(lll_dl_Level_001_mesh_layer_1_vtx_8);
+
+	deltaY = (int)(-0.5 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_lll_dl_Plane_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 120;
@@ -21,7 +42,7 @@ void scroll_lll_dl_Plane_mesh_layer_1_vtx_0() {
 
 void scroll_lll_dl_Plane_011_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 40;
+	int count = 100;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
@@ -61,8 +82,31 @@ void scroll_lll_dl_Plane_012_mesh_layer_1_vtx_0() {
 	currentY += deltaY;
 }
 
+void scroll_lll_dl_Plane_013_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 8;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(lll_dl_Plane_013_mesh_layer_1_vtx_0);
+
+	deltaY = (int)(-0.5 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_lll() {
+	scroll_lll_dl_Level_001_mesh_layer_1_vtx_8();
 	scroll_lll_dl_Plane_mesh_layer_1_vtx_0();
 	scroll_lll_dl_Plane_011_mesh_layer_1_vtx_0();
 	scroll_lll_dl_Plane_012_mesh_layer_1_vtx_0();
+	scroll_lll_dl_Plane_013_mesh_layer_1_vtx_0();
 };
