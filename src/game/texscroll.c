77d06c6848dd7e10b1436b9ff0dfd0647b31926c
Buggy <evanbuggy15@gmail.com>
2025-02-13 15:13:37 +0000

    Tutorial Level work

--- src/game/texscroll.c
+++ src/game/texscroll.c
@@ -17,6 +17,7 @@
 #include "src/game/texscroll/wf_texscroll.inc.c"
 #include "src/game/texscroll/group0_texscroll.inc.c"
 #include "src/game/texscroll/castle_grounds_texscroll.inc.c"
+#include "src/game/texscroll/wdw_texscroll.inc.c"
 void scroll_textures() {
 	if(SCROLL_CONDITION(sSegmentROMTable[0x7] == (uintptr_t)_lll_segment_7SegmentRomStart)) {
 		scroll_textures_lll();
@@ -36,6 +37,10 @@ void scroll_textures() {
 
 	if(SCROLL_CONDITION(sSegmentROMTable[0x4] == (uintptr_t)_group0_mio0SegmentRomStart)) {
 		scroll_textures_group0();
+	}
+
+	if(SCROLL_CONDITION(sSegmentROMTable[0x7] == (uintptr_t)_wdw_segment_7SegmentRomStart)) {
+		scroll_textures_wdw();
 	}
 
 }
