d1384e43c9fc19ea049ec67f6a1887a5656bd20a
Frijoles Y Queso <135540428+FrijolesYQueso@users.noreply.github.com>
2025-02-13 21:53:19 -0600

    Added LLL boss arena, SURFACE_WARP isn't playing nicely though

---
+++ levels/lll/area_2/geo.inc.c
@@ -0,0 +1,34 @@
+#include "src/game/envfx_snow.h"
+
+const GeoLayout lll_area_2_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 49984, 0, 0, lll_dl_Cylinder_016_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout lll_area_2[] = {
+	GEO_NODE_SCREEN_AREA(10, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+	GEO_OPEN_NODE(),
+		GEO_ZBUFFER(0),
+		GEO_OPEN_NODE(),
+			GEO_NODE_ORTHO(100.0000),
+			GEO_OPEN_NODE(),
+				GEO_BACKGROUND(BACKGROUND_FLAMING_SKY, geo_skybox_main),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+		GEO_ZBUFFER(1),
+		GEO_OPEN_NODE(),
+			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 100, 30000, geo_camera_fov),
+			GEO_OPEN_NODE(),
+				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, -49984, 0, 0, -49984, -100, 0, geo_camera_main),
+				GEO_OPEN_NODE(),
+					GEO_BRANCH(1, lll_area_2_geo),
+					GEO_RENDER_OBJ(),
+					GEO_ASM(ENVFX_LAVA_BUBBLES, geo_envfx_main),
+				GEO_CLOSE_NODE(),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
--- levels/lll/texscroll.inc.c
+++ levels/lll/texscroll.inc.c
@@ -147,12 +147,33 @@ void scroll_lll_dl_Plane_026_mesh_layer_1_vtx_0() {
 
 void scroll_lll_dl_Plane_027_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 58;
+	int count = 51;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
 	Vtx *vertices = segmented_to_virtual(lll_dl_Plane_027_mesh_layer_1_vtx_0);
+
+	deltaY = (int)(-0.5 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_lll_dl_Plane_036_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 11;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(lll_dl_Plane_036_mesh_layer_1_vtx_0);
 
 	deltaY = (int)(-0.5 * 0x20) % height;
 
@@ -175,4 +196,5 @@ void scroll_lll() {
 	scroll_lll_dl_Plane_014_mesh_layer_1_vtx_0();
 	scroll_lll_dl_Plane_026_mesh_layer_1_vtx_0();
 	scroll_lll_dl_Plane_027_mesh_layer_1_vtx_0();
+	scroll_lll_dl_Plane_036_mesh_layer_1_vtx_0();
 };
