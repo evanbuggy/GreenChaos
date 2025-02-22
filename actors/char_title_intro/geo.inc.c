682f2e1dbec25175fb8186450527686d2412f67f
Frijoles Y Queso <135540428+FrijolesYQueso@users.noreply.github.com>
2025-02-21 21:29:55 -0600

    Hubsss and intro stuff

--- actors/char_title_intro/geo.inc.c
+++ actors/char_title_intro/geo.inc.c
@@ -26,7 +26,7 @@ const GeoLayout char_title_intro_geo[] = {
 	GEO_OPEN_NODE(),
 		GEO_SHADOW(0, 180, 50),
 		GEO_OPEN_NODE(),
-			GEO_SWITCH_CASE(6, geo_switch_anim_state),
+			GEO_SWITCH_CASE(5, geo_switch_anim_state),
 			GEO_OPEN_NODE(),
 				GEO_NODE_START(),
 				GEO_OPEN_NODE(),
